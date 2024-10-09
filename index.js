e0602bfec8c17cd2072c07898de2523e16619fb0
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-10-08 23:50:23 -0700

    finished assignment

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// The provided course information.
 const CourseInfo = {
   id: 451,
   name: "Introduction to JavaScript",
 };
 
-// The provided assignment group.
 const AssignmentGroup = {
   id: 12345,
-  name: "Fundamentals of JavaScript",
+  name: "Fundam5ntals of JavaScript",
   course_id: 451,
   group_weight: 25,
   assignments: [
@@ -32,14 +30,13 @@ const AssignmentGroup = {
   ],
 };
 
-// The provided learner submission data.
 const LearnerSubmissions = [
   {
     learner_id: 125,
     assignment_id: 1,
     submission: {
       submitted_at: "2023-01-25",
-      score: 47,
+      score: 50,
     },
   },
   {
@@ -47,7 +44,7 @@ const LearnerSubmissions = [
     assignment_id: 2,
     submission: {
       submitted_at: "2023-02-12",
-      score: 150,
+      score: 50,
     },
   },
   {
@@ -55,7 +52,7 @@ const LearnerSubmissions = [
     assignment_id: 3,
     submission: {
       submitted_at: "2023-01-25",
-      score: 400,
+      score: 50,
     },
   },
   {
@@ -76,191 +73,244 @@ const LearnerSubmissions = [
   },
 ];
 
-let result = [];
-let averageItems = [];
+
+
 
 function getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions) {
 
+  let result = [];
+  let averageItems = [];
+  let valid = false; 
   let submission = [];
   let assignments = AssignmentGroup.assignments;
   let current = [];
   let assignmentGrade;
   let deduction;
+  
+  const courseID = CourseInfo.id; 
+  const assignGroupID = AssignmentGroup.course_id; 
  
-  function getAssignments() {
-    let userAssignment;
-    let assignment;
+  try {
+    if (courseID === assignGroupID) {
+      valid = true;
+      function getAssignments() {
+        let userAssignment;
+        let assignment;
+    
+        for (let i = 0; i < assignments.length; i++) {
+          let assignmentObject = assignments[i];
+          assignment = assignments[i].id;
+         
+    
+          for (let i = 0; i < LearnerSubmissions.length; i++) {
+            let user = LearnerSubmissions[i];
+            userAssignment = LearnerSubmissions[i].assignment_id;
+            let learnerID = user.learner_id;
+            
+            let score = (user.submission.score);
+            let totalPossible = (assignmentObject.points_possible);
 
-    for (let i = 0; i < assignments.length; i++) {
-      let assignmentObject = assignments[i];
-      assignment = assignments[i].id;
-     
+            try {
+              if (totalPossible > 0) {
+                valid = true;
 
-      for (let i = 0; i < LearnerSubmissions.length; i++) {
-        let user = LearnerSubmissions[i];
-        userAssignment = LearnerSubmissions[i].assignment_id;
-        let learnerID = user.learner_id; 
-        let score = Number(user.submission.score);
-        let totalPossible = Number(assignmentObject.points_possible);
-       
-
-        while (assignment === userAssignment) {
-
-          let dueDate = assignmentObject.due_at;
-          let dateSubmitted = user.submission.submitted_at;
-          let due = dueDate.toString();
-          let submit = dateSubmitted.toString();
-          let tot = 0; 
-          let poss = 0; 
-
-          switch (true) {
-            case due === submit:  
-
-              tot += score; 
-              poss += totalPossible; 
-              assignmentGrade = Number(score/ totalPossible).toFixed(2);
-              averageItems.push({tot, poss, learnerID, userAssignment, assignmentGrade})
-
-              submission.push([
-                { assignmentObject, user, late: false, assignmentGrade},
-              ]);
-              break;
-
-            case due < submit:
-
-              deduction = totalPossible * 0.1;
-              score = score - deduction;
-              tot += score; 
-              poss += totalPossible; 
-              assignmentGrade = Number(score/ totalPossible).toFixed(3); 
-
-              averageItems.push({tot, poss, learnerID, userAssignment, assignmentGrade})
-
-              submission.push([
-                { assignmentObject, user, late: true, assignmentGrade},
-              ]);
-              break;
-
-            case due > submit:
-
-              const dueYear = Number(due.slice(0, 4));
-              const submitDate = Number(submit.slice(0, 4));
-
-              if (dueYear - submitDate > 1) {
-                break;
-              } else {
-                tot += score; 
-                poss += totalPossible; 
-                assignmentGrade = Number(score/ totalPossible).toFixed(2); 
-
-                if (assignmentGrade == '1.00') {
-                  assignmentGrade = Number(assignmentGrade).toFixed(1)
+                while (assignment === userAssignment) {
+    
+                  let dueDate = assignmentObject.due_at;
+                  let dateSubmitted = user.submission.submitted_at;
+                  let due = dueDate.toString();
+                  let submit = dateSubmitted.toString();
+                  let tot = 0; 
+                  let poss = 0; 
+        
+                  switch (true) {
+                    case due === submit:  
+        
+                      tot += score; 
+                      poss += totalPossible; 
+                      assignmentGrade = Number(score/ totalPossible).toFixed(2);
+                      averageItems.push({tot, poss, learnerID, userAssignment, assignmentGrade})
+        
+                      submission.push([
+                        { assignmentObject, user, late: false, assignmentGrade},
+                      ]);
+                      break;
+        
+                    case due < submit:
+        
+                      deduction = totalPossible * 0.1;
+                      score = score - deduction;
+                      tot += score; 
+                      poss += totalPossible; 
+                      assignmentGrade = Number(score/ totalPossible).toFixed(3); 
+        
+                      averageItems.push({tot, poss, learnerID, userAssignment, assignmentGrade})
+        
+                      submission.push([
+                        { assignmentObject, user, late: true, assignmentGrade},
+                      ]);
+                      break;
+        
+                    case due > submit:
+        
+                      const dueYear = Number(due.slice(0, 4));
+                      const submitDate = Number(submit.slice(0, 4));
+        
+                      if (dueYear - submitDate > 1) {
+                        break;
+                      } else {
+                        tot += score; 
+                        poss += totalPossible; 
+                        assignmentGrade = Number(score/ totalPossible).toFixed(2); 
+        
+                        if (assignmentGrade == '1.00') {
+                          assignmentGrade = Number(assignmentGrade).toFixed(1)
+                        }
+                        averageItems.push({tot, poss, learnerID, userAssignment, assignmentGrade})
+                   
+                        submission.push([
+                          { assignmentObject, user, late: false, assignmentGrade },
+                        ]);
+        
+                        break;
+                      }
+                  }
+                  break;
                 }
-                averageItems.push({tot, poss, learnerID, userAssignment, assignmentGrade})
-           
-                submission.push([
-                  { assignmentObject, user, late: false, assignmentGrade },
-                ]);
 
-                break;
+                
+              }   else {
+                throw new TypeError("Oops, you can't have zero as a total points possible.")
               }
+          
+
+            } catch (error) {
+            console.log(error)
+              return; 
+            }
+           
+    
+           
           }
-          break;
         }
       }
-    }
-  }
 
-  getAssignments();
-
-  function getStudents(arr) {
+      getAssignments()
+    
+      if (valid){
+        function getStudents(arr) {
           return arr.filter((value, index) => arr.indexOf(value) == index);
         }
+         
+      } else {throw new TypeError("Please try again.")}
+    
+    
+     for (let i = 0; i < submission.length; i++) {
+    
+             let item = submission[i];
+            const userId = item[0].user.learner_id;
+    
+            current.push(userId);
 
-  function getAverage() {
-
-
- for (let i = 0; i < submission.length; i++) {
-
-         let item = submission[i];
-        const userId = item[0].user.learner_id;
-
-        current.push(userId);
-
-      }
-     
-    }
-    getAverage();
-
-    let learners = getStudents(current);
-    let position = learners.length; 
-
-    learners.forEach((learner) => {
-      let temp = learner; 
-      let tempTot = 0; 
-      let tempPoss = 0; 
-
+          }
+         
+    
+        let learners = getStudents(current);
+        let position = learners.length; 
+    
+        learners.forEach((learner) => {
+          let temp = learner; 
+          let tempTot = 0; 
+          let tempPoss = 0; 
+    
+          for (let i = 0; i < averageItems.length; i++) {
+            if (averageItems[i].learnerID === temp) {
+              tempTot += averageItems[i].tot; 
+              tempPoss += averageItems[i].poss;
+              i + position;
+            } 
+          }
+    
+          let average = (tempTot / tempPoss); 
+          averageItems.forEach((item) => {
+            if (item.learnerID === temp) {
+          
+            item.average = average; 
+        
+            }
+          })
+    
+        })
+    
+    
       for (let i = 0; i < averageItems.length; i++) {
-        if (averageItems[i].learnerID === temp) {
-          tempTot += averageItems[i].tot; 
-          tempPoss += averageItems[i].poss;
-          i + position;
+        let id = averageItems[i].learnerID; 
+        let avg = averageItems[i].average; 
+        let assignGrade = averageItems[i].assignmentGrade; 
+     
+        let grade = assignGrade; 
+        let grade2 = ``; 
+    
+        let gradeItem = grade.split()
+    
+        gradeItem.forEach((grade, index) => {
+          let assignGrade2 = (averageItems[i + 2])
+    
+          if (assignGrade2 !== undefined) {
+    
+            grade2 = Number(grade2 += assignGrade2.assignmentGrade); 
+          }
+          let array = ((grade.split()))
+    
+         grade = Number(`${array[index]}`); 
+       
+        })
+    
+        let tempObj = {}; 
+        let num1 = Number(1); 
+        let num2 = Number(2) ;
+      
+        if (!result.includes(id)) {
+    
+          if (grade2 !== "") {
+    
+            tempObj = { id: Number(id), avg: Number(avg), [num1] : Number(grade), [num2] : Number(grade2)  }
+            result.push(tempObj); 
+    
+           }
         } 
       }
 
-      let average = (tempTot / tempPoss); 
-      averageItems.forEach((item) => {
-        if (item.learnerID === temp) {
-      
-        item.average = average; 
+      // I was trying to validate my inputs for average and id to make sure that they are numbers and if they aren't then I was going to throw an error. I was able to validate that the course_id matches and that the points possible is above 0. 
     
-        }
-      })
+    //  let objectNums = Object.values(result); 
 
-    })
-
-
-  for (let i = 0; i < averageItems.length; i++) {
-    let id = averageItems[i].learnerID; 
-    let avg = averageItems[i].average; 
-    let assignGrade = averageItems[i].assignmentGrade; 
- 
-    let grade = assignGrade; 
-    let grade2 = ``; 
-
-    let gradeItem = grade.split()
-
-    gradeItem.forEach((grade, index) => {
-      let assignGrade2 = (averageItems[i + 2])
-
-      if (assignGrade2 !== undefined) {
-
-        grade2 = grade2 += assignGrade2.assignmentGrade; 
-      }
-      let array = ((grade.split()))
-
-     grade = `${array[index]}`; 
-   
-    })
-
-    let tempObj = {}; 
-    let num1 = 1; 
-    let num2 = 2 ;
+    // objectNums.forEach((number) => {
+    //   try {
+    //     if (number.avg || number.id !== 'NAN') {
+    //       throw new TypeError('Error, please use numbers')
+    //     }
+    //   } catch (err) {
+    //     console.log(err)
+    //   }
+    // })
+        
+    
   
-    if (!result.includes(id)) {
+      return result;
+       
+    } else {
+      throw new Error('Oops, your course_id does not match!');
+    }
+  } catch ({name, message}) {
+    console.log(message)
+  } 
+  }  
 
-      if (grade2 !== "") {
+ 
 
-        tempObj = { id: id, avg: avg, [num1] : grade, [num2] : grade2  }
-        result.push(tempObj); 
 
-       }
-    } 
-  }
 
-  return result;
 
-}
-
-let myResult = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-console.log(myResult)
+  let myResult = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+  console.log(myResult);
